98f09e16ffe804f0e7bdbec2ed706d6d896b5138
AdityaSonule <aditya.sonule07@gmail.com>
2026-06-21 18:10:48 +0530

    added frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,7 +14,7 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();*/
+reportWebVitals();
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -32,4 +32,21 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';   // ✅ changed
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// ✅ NEW way (React 18+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 reportWebVitals();
